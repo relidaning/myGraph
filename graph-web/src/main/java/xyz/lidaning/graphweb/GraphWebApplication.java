fd6d9c1f8cc349c885c18618ab17cc9f608a7955
lidaning <453882101@qq.com>
2022-07-21 17:53:29 +0800

    graph web demo

---
+++ graph-web/src/main/java/xyz/lidaning/graphweb/GraphWebApplication.java
@@ -0,0 +1,13 @@
+package xyz.lidaning.graphweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphWebApplication.class, args);
+    }
+
+}
